d72493c7e6899fc14136b24c6b96a7980ff247b8
Nematov Muhammadjon <123355875+SMILE-NM@users.noreply.github.com>
2023-05-15 00:16:39 +0500

    50% Work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { clear } from "@testing-library/user-event/dist/clear";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+
+    //Add the todo to the list 
+    setList([...list, newTodo]);
+
+    // clear input box
+    setInput("");
+  };
+
+
+
+  return (
+    < div >
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => addTodo(input)}>Add</button>
+    </div>
+
+    
+  );
+
+}
 export default App;
