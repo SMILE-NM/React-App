1b8c26f74fe49ade7c99c51ce68bce46c0a886b3
Nematov Muhammadjon <nematovmz2003@gmail.com>
2023-05-18 11:55:12 +0500

    Full Todo List

--- src/App.js
+++ src/App.js
@@ -1,38 +1,60 @@
 import { clear } from "@testing-library/user-event/dist/clear";
 import React, { useState } from "react";
+import "./App.css";
 
 function App() {
 
-  const [list, setList] = useState([]);
-  const [input, setInput] = useState("");
 
-  const addTodo = (todo) => {
+  
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState(10);
+
+  // setInput(input+10);
+  
+ 
+  const addTodo = (todoInput) => {
+    if(todoInput !== "")
+    {
     const newTodo = {
       id: Math.random(),
-      todo: todo,
+      values: todoInput,
     };
-
+ 
     //Add the todo to the list 
     setList([...list, newTodo]);
+
+  }
+
 
     // clear input box
     setInput("");
   };
 
+  const deleteTodo = (id) => {
+    const newList = list.filter((todoInput) => todoInput.id !== id);
 
+    setList(newList);
+  }
 
   return (
-    < div >
+    <div className="container">
       <h1>Todo List</h1>
       <input
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <button onClick={() => addTodo(input)}>Add</button>
-    </div>
+      <button className="add" onClick={() => addTodo(input)}>Add</button>
 
-    
+      <ul>
+        {list.map((todoInput) => (
+          <div className="todo" key={todoInput.id}>
+            <li>{todoInput.values}</li>
+            <button onClick={() => deleteTodo(todoInput.id)}>&times;</button>
+          </div>
+        ))}
+      </ul>
+    </div>
   );
 
 }
